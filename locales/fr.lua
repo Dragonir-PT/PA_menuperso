a2e3c5d64558b91c4b201f3a621d9dd2725e2353
dragonir44 <th.flavien@hotmail.fr>
2020-09-19 18:35:33 +0200

    Add file

---
+++ locales/fr.lua
@@ -0,0 +1,187 @@
+Locales['fr'] = {
+    ['me'] = "Me concernant",
+
+    --inventory
+    ['inventory'] = "Inventaire",
+    ['inv_label'] = "%s (%s)",
+    ['inv_desc'] = "%s Kg / %s Kg",
+
+    ['use'] = "Utiliser",
+    ['give'] = "Donner",
+    ['give_label'] = "%s (%s)",
+    ['amount'] = "Montant",
+    ['give_car'] = "Vous ne pouvez pas donner ~y~%s~s~ dans une voiture",
+    ['cant_give'] = "Vous ne pouvez pas donner cette objet",
+    ['nobody'] = "Personne à proximité",
+    ['drop'] = "Jeter",
+    ['drop_car'] = "%s ne peux pas être jeter depuis une voiture",
+    ['cant_drop'] = "%s ne peux pas être jeter",
+
+    ['quantity'] = "Quantité",
+    ['invalid_quantity'] = "Montant invalide",
+    ['invalid_entry'] = "Entrée invalide",
+
+    --weapon
+    ['weapon'] = "Armes",
+
+    --wallet
+    ['wallet'] = "Portefeuille",
+    ['job_label'] = "Métier : %s - %s",
+    ['gang_label'] = "Organisation : %s - %s",
+    ['money_label'] = "%s : ~g~%s$",
+    ['black_label'] = "%s : ~g~%s$",
+    ['bill'] = "Factures",
+    ['paper'] = "Papiers",
+    ['show_idcard'] = "Montrer Carte d'identité",
+    ['look_idcard'] = "Voir carte d'identité",
+    ['show_licence'] = "Montrer permis de conduire",
+    ['look_licence'] = "Voir permis de conduire",
+    ['show_weapon'] = "Montrer PPA",
+    ['look_weapon'] = "Voir PPA",
+
+    --car key
+    ['car_key'] = "Clés",
+    ['make_double'] = "Faire un double",
+    ['give_key'] = "Donner les clés",
+
+    --hud
+    ['hud'] = "HUD",
+    ['show_hud'] = "Afficher",
+    ['hide_hud'] = "Cacher",
+    ['cinematic_hud'] = "Cinématique",
+
+    ['anim'] = "Animation",
+
+    ['gps'] = "GPS",
+    ['gps_notif'] = "GPS: ~b~%s",
+
+    --vehicle
+    ['vehicle_manager'] = "Gestion véhicule",
+    ['info_veh'] = "Information véhicule",
+    ['engine_on_off'] = "~g~Allumer~s~/~r~Éteindre~s~ le moteur",
+    ['inside_veh'] = "Vous devez être dans un véhicule",
+    ['open_door'] = "~g~Ouvrir~s~/~r~Fermer~s~ la porte",
+    ['open_window'] = "~g~Ouvrir~s~/~r~Fermer~s~ la fenêtre",
+    ['open_capot'] = "~g~Ouvrir~s~/~r~Fermer~s~ le capot",
+    ['open_trunk'] = "~g~Ouvrir~s~/~r~Fermer~s~ le coffre",
+    --state
+    ['perfect'] = "~g~Parfait",
+    ['danger'] = "~y~Danger",
+    ['critical'] = "~r~Critique",
+    ['dead'] = "~u~HS",
+
+    --door and window
+    ['front_left'] = "avant gauche",
+    ['front_right'] = "avant droite",
+    ['back_left'] = "arrière gauche",
+    ['back_right'] = "arrière droite",
+
+    --class
+    ['compact'] = "Compacts",
+    ['sedan'] = "Berline",
+    ['suv'] = "SUV",
+    ['coupe'] = "Coupés",
+    ['muscle'] = "Muscle",
+    ['sportclassic'] = "Sportive classique",
+    ['sports'] = "Sportive",
+    ['super'] = "Super sportive",
+    ['motorbike'] = "Moto",
+    ['offroad'] = "Tout-terrain",
+    ['industrial'] = "Industrielle",
+    ['util'] = "Utilistaire",
+    ['van'] = "Van",
+    ['bike'] = "Vélo",
+    ['boat'] = "Bâteau",
+    ['heli'] = "Hélicoptère",
+    ['plane'] = "Avion",
+    ['service'] = "Service",
+    ['emergency'] = "Secours",
+    ['army'] = "Militaire",
+    ['commercial'] = "Commercial",
+    ['train'] = "Train",
+
+    --info veh
+    ['veh_name'] = "Nom du véhicule",
+    ['class_name'] = "Classe",
+    ['plate_number'] = "Plaque",
+    ['oil'] = "Carburant",
+    ['seat'] = "Nombre de place passager",
+    ['engine_state'] = "Etat moteur",
+    ['body_state'] = "Etat carrosserie",
+    ['tank_state'] = "Etat reservoir",
+
+    --society
+    ['society_manager'] = "Gestion entreprise",
+    ['society_bank'] = "Compte entreprise",
+    ['recruit'] = "Recruter",
+    ['fire'] = "Licensier",
+    ['promote'] = "Promouvoir",
+    ['retrograde'] = "Rétrograder",
+
+    --gang
+    ['gang_manager'] = "Gestion organisation",
+    ['gang_bank'] = "Compte organisation",
+
+    ['save_pos'] = "Sauvegarder la position",
+    ['notif_save'] = "~b~Position sauvegarder",
+
+    --admin
+    ['admin'] = "Administration",
+
+    ['tp'] = "TP",
+    ['summon'] = "TP sur soi",
+    ['marker'] = "TP sur marqueur",
+    ['goto'] = "TP sur joueur",
+    ['coord'] = "TP sur coord",
+
+    ['player'] = "Joueur",
+    ['noclip'] = "No clip",
+    ['god'] = "God mode",
+    ['ghost'] = "Mode fantôme",
+
+    ['item_list'] = "Objet",
+    ['item_list_desc'] = "Nom de l'objet : %s \nPoids de l'objet : %s Kg",
+    ['give_item'] = "Vous vous êtes donné ~y~x%s ~b~%s",
+
+    ['job_list'] = "Job",
+    ['grade_list_desc'] = "Nom : %s\nId : %s",
+    ['new_grad_notif'] = "Vous êtes maintenant %s au %s",
+
+    ['gang_list'] = "Gang",
+
+    ['vehicle'] = "Véhicule",
+    ['repair_veh'] = "Réparer véhicule",
+    ['spawn_veh'] = "Faire apparaître un véhicule",
+    ['flip_veh'] = "Retourner le véhicule",
+    ['give_admin_key'] = "Récupérer les clés du véhicule",
+    ['owned_veh'] = "Ajouter cette voiture à votre garage",
+    ['refuel'] = "Remettre de l'essence",
+    ['gofast'] = "Ca envoie du gros",
+
+    ['other'] = "Autre",
+    ['give_money'] = "S'octroyer de l'argent",
+    ['give_bank'] = "S'octroyer de l'argent (banque)",
+    ['give_black'] = "S'octroyer de l'argent (Sale)",
+    ['show_coord'] = "Afficher/Cacher coordonnées",
+    ['show_name'] = "Afficher/Cacher noms des joueurs",
+    ['change_name'] = "Changer l'apparence",
+    ['save_skin'] = "Sauvegarder l'apparence",
+
+    --popup
+    ['job_not_register'] = "~r~Votre métier n'a pas de menu enregistrer",
+    ['enter_id'] = "~b~Entrez l'ID du joueur...",
+    ['tp_on_marker'] = "Téléporté sur le marqueur !",
+    ['no_marker'] = "Pas de marqueur sur la carte !",
+    ['enter_pos'] = "~b~Entrez les coordonées...",
+    ['no_clip_on'] = "Noclip ~g~activé",
+    ['no_clip_off'] = "Noclip ~r~désactivé",
+    ['god_mode_on'] = "Mode invincible ~g~activé",
+    ['god_mode_off'] = "Mode invincible ~r~désactivé",
+    ['ghost_mode_on'] = "Mode fantôme ~g~ activé",
+    ['ghost_mode_off'] = "Mode fantôme ~r~ désactivé",
+    ['enter_veh_name'] = "~b~Entrez le nom du véhicule...",
+    ['veh_fliped'] = "Véhicule retourné",
+    ['notif_playername'] = "Ouvrir/Fermer le menu pause pour afficher les noms",
+
+    ['coords'] = "~r~X~s~: %s ~b~Y~s~: %s ~g~Z~s~: %s ~y~Rotation~s~: %s",
+}
