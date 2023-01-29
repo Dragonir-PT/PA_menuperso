d62aeee9ed5febe4e9dbf6bc782396b62be325a3
Dragonir44 <dragonir44@gmail.com>
2023-01-29 17:28:26 +0100

    fix ragdoll

--- client/anim/ragdoll.lua
+++ client/anim/ragdoll.lua
@@ -1,8 +1,13 @@
+local isRagdoll = false
 RegisterNetEvent('Drago_menuperso:ragdoll')
 AddEventHandler('Drago_menuperso:ragdoll',function()
     local playerPed = PlayerPedId()
     local vehicle = GetVehiclePedIsIn(playerPed,false)
+    isRagdoll = not isRagdoll
     if not IsPedInVehicle(playerPed, vehicle, false) then
-        SetPedToRagdoll(GetPlayerPed(-1), 1000, 1000, 0, false, false, false)
+        while isRagdoll do
+            Wait(0)
+            SetPedToRagdoll(PlayerPedId(), 1000, 1000, 0, false, false, false)
+        end
     end
 end)
